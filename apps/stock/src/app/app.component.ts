107c5cd14e024a7d852fa720f9d019872fc4e25f
Dimitri De Kerf <dimidekerf@hotmail.com>
2019-12-02 21:17:52 +0100

    feat(stock): show stock of phones in table

--- apps/stock/src/app/app.component.ts
+++ apps/stock/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Phone } from '@nx-phone/model';
 
 @Component({
   selector: 'nx-phone-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stock';
+export class AppComponent implements OnInit {
+  phones$: Observable<Phone[]>;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.phones$ = this.http.get<Phone[]>('/api/phones');
+  }
 }
